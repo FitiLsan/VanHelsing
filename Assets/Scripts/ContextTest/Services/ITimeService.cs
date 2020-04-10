cbc3e9b67a65f50481e89b60d7379c9ee7936889
laic00 <dich0000@mail.ru>
2020-04-11 00:41:44 +0500

    add TimeService

---
+++ Assets/Scripts/ContextTest/Services/ITimeService.cs
@@ -0,0 +1,14 @@
+﻿namespace ExampleTemplate
+{
+    public interface ITimeService
+    {
+        float DeltaTime();
+        float UnscaledDeltaTime();
+        float FixedDeltaTime();
+        float RealtimeSinceStartup();
+        float GameTime();
+        long Timestamp();
+        void SetTimeScale(float timeScale);        
+        void ResetDeltaTime();
+    }
+}
---
+++ Assets/Scripts/ContextTest/Services/UnityTimeService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+ 
+
+namespace ExampleTemplate
+{
+    public sealed class UnityTimeService : Service, ITimeService
+    {
+        #region Fields
+
+        private int _deltaTimeResetFrame;
+
+        #endregion
+
+
+        #region Methods
+        
+        public float DeltaTime() => _deltaTimeResetFrame == Time.frameCount ? 0.0f : Time.deltaTime;
+        public float UnscaledDeltaTime() => _deltaTimeResetFrame == Time.frameCount ? 0.0f : Time.unscaledDeltaTime;
+        public float FixedDeltaTime() => _deltaTimeResetFrame == Time.frameCount ? 0.0f : Time.fixedDeltaTime;
+        public float RealtimeSinceStartup() => Time.realtimeSinceStartup;
+        public float GameTime() => Time.time;
+        public long Timestamp() => DateTime.Now.ToUnixTimestamp(); //todo UtcNow
+        public void SetTimeScale(float timeScale) => Time.timeScale = timeScale;
+        public void ResetDeltaTime() => _deltaTimeResetFrame = Time.frameCount;
+
+        #endregion
+    }
+}
