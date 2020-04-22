3653904eef74f7bc9b7f2c30da1f680baa59d722
Rogier <tujhybr728@mail.ru>
2020-04-22 21:23:54 +0300

    начало

---
+++ Assets/Scripts/TestingITick/TestDayNigthShift/DayNightShiftStruct.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+
+    class DayNightShiftStruct
+
+{
+
+
+    #region Fields
+
+    public float secondsInFullDay = 120f;
+
+
+
+    #endregion
+
+
+}
+
---
+++ Assets/Scripts/TestingITick/TestDayNigthShift/DayNigthShiftData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+    class DayNigthShiftData
+{
+
+
+    #region Fields
+
+    public DayNightShiftStruct DayNightShiftStruct;
+
+    #endregion
+
+    #region Metods
+
+    public void Move(Transform transform, Transform target, float speed)
+    {
+
+    }
+
+
+    #endregion
+}
