8a94ae8eabc2bd7d1a46450e07ca757d71f096fb
TonyZub <zub0296@mail.ru>
2020-05-21 02:11:55 +0700

    Created branch only for sphere test

--- Assets/Scripts/Controllers/BaseControllers/MainControllers.cs
+++ Assets/Scripts/Controllers/BaseControllers/MainControllers.cs
@@ -6,20 +6,8 @@
 
         public MainControllers(GameContext context)
         {
-            Add(new CharacterInitilizeController(context));
-            Add(new GiantMudCrabInitilizeController(context));
-            Add(new DialogueSystemInitializeController(context));
-            Add(new StartDialogueInitializeController(context));
-            Add(new InitializeInteractableObjectController(context));
-            Add(new QuestInitializeController(context));
-            Add(new GiantMudCrabController(context));
-            Add(new TargetController(context));
-            Add(new InputController(context));
-            Add(new CharacterController(context));
-            Add(new DialogueSystemController(context));
-            Add(new StartDialogueController(context));
-            Add(new DialogueTriggerController(context));
-            Add(new QuestController(context));
+            Add(new SphereInitilizeController(context));
+            Add(new SphereController(context));
         }
 
         #endregion
--- Assets/Scripts/To_Refactoring/Inventory/WeaponType.cs
+++ Assets/Scripts/To_Refactoring/Inventory/WeaponType.cs
@@ -2,6 +2,7 @@
 {
     public enum WeaponType
     {
+        None,
         MeleeCutting,
         MeleeSmashing,
         RangeShooting,
