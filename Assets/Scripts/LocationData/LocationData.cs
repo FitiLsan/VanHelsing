c652cd27d1cebcc02f1e966e99bee425f7376aa6
Михаил <shipov-mihail@mail.ru>
2020-06-10 15:56:48 +0300

    WIP: wip

---
+++ Assets/Scripts/LocationData/LocationData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewData", menuName = "CreateData/LocationData", order = 0)]
+public class LocationData : ScriptableObject
+{
+    #region Fields
+
+    public List<GameObject> SpawnPoints = new List<GameObject>();
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
---
+++ Assets/Scripts/LocationData/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    #region Fields
+
+    public Vector3 ObjectVector;
+    public List<GameObject> SpawnDayPrefabs;
+    public List<GameObject> SpawnNightPrefabs;
+    public bool IsNight = false;
+
+    #endregion
+
+    #region Awake
+
+    private void Awake()
+    {
+        transform.position = ObjectVector;
+        if (IsNight == true)
+        {
+            foreach (GameObject prefabs in SpawnDayPrefabs)
+            {
+                prefabs.GetComponent<OnAwake>
+            }
+        }
+    }
+
+    #endregion
+}
