62b7c449078d13fc09f8bbff54f7c07e7ce8bca3
Михаил <shipov-mihail@mail.ru>
2020-06-30 22:11:30 +0300

    WIP: Added boss SO

---
+++ Assets/Scripts/Enum/BossEnum/BossSOPattern.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "BossSO")]
+public class BossSOPattern : ScriptableObject
+{
+    #region Enums
+
+    [Header("Boss Type")]
+    public BossTypes BossTypes;
+
+    [Header("Boss Subtypes")]
+    public AnimaliaSubtypes AnimaliaSubtypes;
+    public MentaliaSubtypes MentaliaSubtypes;
+    public ParazitaliaSubtypes ParazitaliaSubtypes;
+    public SpiritualiaSubtypes SpiritualiaSubtypes;
+    public VegetaliaSuptypes VegetaliaSuptypes;
+
+    [Header("Boss Size")]
+    public BossSize Size;
+
+    [Header("Boss Sociality")]
+    public BossSociality Sociality;
+
+    [Header("Boss Habitates")]
+    public BossHabitat[] Habitat;
+
+    [Header("Objects of Attraction")]
+    public AttractionOrAvoidance[] Attraction;
+
+    [Header("Objects of Avoidance")]
+    public AttractionOrAvoidance[] Avoidance;
+
+    [Header("Weakness Material")]
+    public WeaknessOrImmunityMaterial[] Weakness;
+
+    [Header("Immunity Material")]
+    public WeaknessOrImmunityMaterial[] Immunity;
+
+    [Header("Interaction With The Environment")]
+    public InteractionWithTheEnvironment[] Interaction;
+
+    [Header("Behavior If Boss Sees A Hunter")]
+    public IfSeesTheHunter IfSeesTheHunter;
+
+    [Header("Time, When Boss Is Active")]
+    public ActivityTime[] Activity;
+
+    [Header("Nesting Place")]
+    public NestingGrounds[] Nestings;
+
+    [Header("Way To Move")]
+    public WayToMove WayToMove;
+
+    #endregion
+}
