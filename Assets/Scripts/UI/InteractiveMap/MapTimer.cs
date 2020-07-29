2f2ffa13704a3d39993456554a49e9f1a519b11e
Sainell <>
2020-07-30 00:58:15 +0300

    fix
    add color text in db
    fix timer by distance

--- Assets/Scripts/UI/InteractiveMap/MapTimer.cs
+++ Assets/Scripts/UI/InteractiveMap/MapTimer.cs
@@ -12,29 +12,30 @@ namespace BeastHunter
         public int CurrentMinutes;
         public Text Hour;
         public Text Minute;
+        public int CurrentDistance;
         private void Awake()
         {
-            ToComeInPlace.ToComeInPlaceObjEvent += OnPlaceTransfer;
-
+            PlaceInteractiveField.PositionCheckEvent += OnPlaceDistance;
             Hour = gameObject.transform.Find("Hours").GetComponent<Text>();
             Minute = gameObject.transform.Find("Minuts").GetComponent<Text>();
 
             CurrentHour = Convert.ToInt16( gameObject.transform.Find("Hours").GetComponent<Text>().text);
             CurrentMinutes = Convert.ToInt16(gameObject.transform.Find("Minuts").GetComponent<Text>().text);
         }
-
-        private void OnPlaceTransfer()
+        private void OnPlaceDistance(int distance)
         {
-            CurrentMinutes +=30 ;
-            if(CurrentMinutes>=60)
+            CurrentDistance = distance;
+            CurrentMinutes += CurrentDistance * 2;
+            if (CurrentMinutes >= 60)
             {
-                CurrentHour++;
-                CurrentMinutes = 0;
+                CurrentHour += CurrentMinutes / 60;
+                
+                CurrentMinutes = CurrentMinutes % 60;
             }
 
             Minute.text = string.Format($"{CurrentMinutes:d2}");
             Hour.text = string.Format($"{CurrentHour:d2}");
-            
+            Debug.Log($"distance - {distance} time {CurrentMinutes}");
         }
     }
 }
