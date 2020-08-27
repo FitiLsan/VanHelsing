33774639b85e03ce5566e03d26f5f1da8261a77f
RinaSatsu <Vanilin6543@gmail.com>
2020-08-27 12:46:19 +0300

    Merge pull request #64 from ProdavecBurrito/code/shipo/time-ui
    code/shipov/circle-ui
    # Conflicts:
    #	Assets/Scenes/UI Test.unity
    #	Assets/Scripts/UI/TimeSkip.cs

---
+++ Assets/Scripts/UI/TimeSkip.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public sealed class TimeSkip : MonoBehaviour
+{
+    #region Constant
+
+    private const int maxHours = 24;
+    private const int devider = 2;
+    private const int halfDay = 12;
+    private const string RainWeather = "Rain";
+    private const string SunWeather = "Sunny";
+    private const string FogWeather = "Fog";
+
+    #endregion
+
+
+    #region Fields
+
+    private int tempHour;
+    private int firstHour;
+    private int secondHour;
+    public Text weatherTxt;
+    public Text hoursTxt;
+    public Image image;
+    public List<Sprite> weather;
+    public Slider hoursSlider;
+
+    #endregion
+
+
+    #region Methods
+
+    public void TimeChanged()
+    {
+        int time = (int)hoursSlider.value;
+        hoursTxt.text = hoursSlider.value.ToString();
+        image.sprite = WeatherPlace(firstHour, secondHour, time);
+    }
+
+    public Sprite WeatherPlace(int firstHour, int secondHour, int time)
+    {
+        if (time < firstHour)
+        {
+            weatherTxt.text = FogWeather;
+            return weather[0];
+        }
+        else if (time < secondHour)
+        {
+            weatherTxt.text = SunWeather;
+            return weather[1];
+        }
+        else
+        {
+            weatherTxt.text = RainWeather;
+            return weather[2];
+        }
+    }
+
+    private void Awake()
+    {
+        WeatherHours();
+        TimeChanged();
+    }
+
+    public void WeatherHours()
+    {
+        firstHour = Random.Range(devider, maxHours - devider);
+        Debug.Log($"Первый час: {firstHour}");
+
+        if (firstHour <= halfDay)
+        {
+            secondHour = Random.Range(firstHour + devider, maxHours - devider);
+            Debug.Log($"Второй час: {secondHour}");
+        }
+        else
+        {
+            tempHour = Random.Range(devider, firstHour - devider);
+            Debug.Log(tempHour);
+            secondHour = firstHour;
+            Debug.Log($"Второй час: {secondHour}");
+            firstHour = tempHour;
+        }
+    }
+
+    #endregion
---
+++ Assets/Scripts/UI/WeaponCircle.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class WeaponCircle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    #region PrivateFields
+
+    private Image _parentImage;
+    private Image _childImage;
+
+    private Color _parentAlfa;
+    private Color _childAlfa;
+
+    private const float PARENT_IMAGE_NON_DEDICATED_ALFA = 0.3f;
+    private const float CHILD_IMAGE_NON_DEDICATED_ALFA = 0.4f;
+    private const float PARENT_IMAGE_DEDICATED_ALFA = 0.6f;
+    private const float CHILD_IMAGE_DEDICATED_ALFA = 0.7f;
+
+
+    private const float PARENT_IMAGE_SCALE_NON_DEDICATED = 0.7f;
+    private const float CHILD_IMAGE_SCALE_NON_DEDICATED = 0.75f;
+    private const float IMAGE_DEDICATED_SCALE = 0.85f;
+
+
+    #endregion
+
+
+    #region Fields
+
+    public Text txt;
+    public string weaponTxt;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Awake()
+    {
+        _parentImage = GetComponent<Image>();
+        _parentAlfa = _parentImage.color;
+        _childImage = GetComponentInChildren<Image>();
+        _childAlfa = _parentImage.color;
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (_parentImage != null && _childImage != null)
+        {
+            _parentAlfa.a = PARENT_IMAGE_DEDICATED_ALFA;
+            _childAlfa.a = CHILD_IMAGE_DEDICATED_ALFA;
+            _parentImage.rectTransform.localScale = new Vector3(IMAGE_DEDICATED_SCALE, IMAGE_DEDICATED_SCALE, IMAGE_DEDICATED_SCALE);
+            _parentImage.color = _parentAlfa;
+            _childImage.rectTransform.localScale = new Vector3(IMAGE_DEDICATED_SCALE, IMAGE_DEDICATED_SCALE, IMAGE_DEDICATED_SCALE);
+            _childImage.color = _childAlfa;
+            txt.text = weaponTxt;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (_parentImage != null && _childImage != null)
+        {
+            _parentAlfa.a = PARENT_IMAGE_NON_DEDICATED_ALFA;
+            _childAlfa.a = CHILD_IMAGE_NON_DEDICATED_ALFA;
+            _parentImage.rectTransform.localScale = new Vector3(PARENT_IMAGE_SCALE_NON_DEDICATED, PARENT_IMAGE_SCALE_NON_DEDICATED, PARENT_IMAGE_SCALE_NON_DEDICATED);
+            _parentImage.color = _parentAlfa;
+            _childImage.rectTransform.localScale = new Vector3(CHILD_IMAGE_SCALE_NON_DEDICATED, CHILD_IMAGE_SCALE_NON_DEDICATED, CHILD_IMAGE_SCALE_NON_DEDICATED);
+            _childImage.color = _childAlfa;
+        }
+    }
+
+    #endregion
+
+}
